e7f20bb8a4490a1303f2071788b0338cf7b55b42
Neko-Sato <hisatosi35350@gmail.com>
2024-03-27 13:53:43 +0900

    update

---
+++ ex03/Weapon.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Weapon.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/27 13:13:23 by hshimizu          #+#    #+#             */
+/*   Updated: 2024/03/27 13:45:21 by hshimizu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef __WEAPON_H__
+#define __WEAPON_H__
+
+#include <iostream>
+
+class Weapon
+{
+private:
+	std::string _type;
+
+public:
+	Weapon(std::string type);
+	~Weapon();
+
+	std::string const &getType() const;
+	void setType(std::string const &type);
+};
+
+#endif
